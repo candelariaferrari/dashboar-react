abc451f19027cefa8a12d7b9fea5153f0cbf6bf6
Candelaria Ferrari <ferrari.candelaria@gmail.com>
2022-06-07 16:07:22 -0300

    header api

--- dashboard-holaalmundo/src/components/nuestro-dashboard/Header/Header.js
+++ dashboard-holaalmundo/src/components/nuestro-dashboard/Header/Header.js
@@ -4,22 +4,22 @@ import person from '../../../assets/images/circle-user-solid.svg';
 import '../../../assets/css/myStyle.css';
 
 function Header() {
-  const [users, setUsers] = useState("");
+  const [users, setUsers] = useState(""); //estado
   useEffect(() => {
-    const url = 'http://localhost:3000/api/users';
-    const fetchData = async () => {
+    const url = 'http://localhost:3000/api/users'; //definimos url
+    const fetchDataUsers = async () => { //funcion asincrona
       try {
-        const response = await fetch(url);
-        const json = await response.json();
-       
+        const response = await fetch(url); //espere a hacer la busqueda
+        const json = await response.json(); //lo transformamos en json
+        console.log(json); //array completo
         console.log("hoola" + " " + json.data[0].name);
-        setUsers(json.data[0].name);
+        setUsers(json.data[0].name);//lo guardamos en el estado
       } catch (error) {
         console.log("error", error);
       }
     };
 
-    fetchData();
+    fetchDataUsers();
   }, []);
   return (
     <div className='container-fluid black'>
